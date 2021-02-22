c88670ec6bdbc365583210b26a31f942068d518a
kraci2015 <78047268+kraci2015@users.noreply.github.com>
2021-02-22 17:12:27 +0200

    Create database.php

---
+++ database.php
@@ -0,0 +1,86 @@
+
+	{
+	
+	"kryesore" : [
+		{
+			
+			"mtu":"http://tvshqip.xyz/gog.php",
+
+			"mtuu":"https://raw.githubusercontent.com/mainape/alapp/main/playlist.m3u",
+			"appid":"ca-app-pub-6303685172759677~4345612576",
+			
+			"adbaner":"ca-app-pub-8336603601915057/2875110002a",
+			
+			"adint":"ca-app-pub-8336603601915057/9987313267a",
+			
+			"admob":"admob",
+			
+			"adt":"mmm",
+			
+			"adurl":"h",
+			
+			"adcount":"20",
+			
+			"adtipi":"admob",
+			
+			"lista":"http",
+			
+			"contain":"miri;l",
+			
+			"digis":"https://raw.g",
+			
+			"ends":"miri",
+			
+			"filma":"https://www.filma24.ac",
+			
+			"filmac":"0",
+			
+			"kryesor":"https://raw.githubusercontent.com/",
+			
+			"nder":"https://raw.githubusercontent.com/",
+			
+			"shqiptare":"https://raw.githubusercontent.com/",
+			
+			"sign":"JNg31JOZ+GLwhNULkihPfhnOLas=",
+			
+			"slide":"https://raw.githubusercontent.com/",
+			
+			"sport":"https://raw.githubusercontent.com/",
+			
+			"tring":"https://raw.githubusercontent.com/",
+			
+			"adx":"https://raw.githubusercontent.com/",
+			
+			"adxpa":"1111",
+			
+			"fireurl":"https://tawk.to/chat/5e73dd868d24fc226588c10d/default",
+			
+			"updatever":"1.2",
+			
+			"updateurl":"https://t.me/alketashqip",
+			
+			"server":{
+				
+				"alba":{
+					
+					"arra":"https://fupfap.com/albbox372admin/api.php",
+					
+					"arrch":"",
+					
+					"arrusg":"jo",
+					
+					"usg":"jo",
+					
+					"auth":"Basic ODc5NzMxMzBiOmQwZDhmOTA5MzRk",
+					
+					"tokenurl":"https://fupfap.com/albplayer/t0k3n.php"
+					
+				}
+				
+			}
+			
+		}
+		
+	]
+	
+}
