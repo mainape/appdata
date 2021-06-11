fd5977a31923dce887f5a5cc0e142056426259e6
kraci2015 <78047268+kraci2015@users.noreply.github.com>
2021-06-11 23:02:48 +0300

    Update alvision.php

--- alvision.php
+++ alvision.php
@@ -1,1 +1,126 @@
 
+
+
+	{
+	
+	"kryesore" : [
+		{
+			
+			"mtu":"https://raw.githubusercontent.com/free-greek-iptv/greek-iptv/master/android.m3u",
+			
+			"appid":"ca-app-pub-7683993442880393~6499002234",
+			
+			"adbaner":"ca-app-pub-7683993442880393/3931214376",
+			
+			"adint":"ca-app-pub-7683993442880393/2618132707",
+			
+			"admob":"admob",
+			
+			"adt":"mmmmmm",
+			
+			"adurl":"http://51.15.7.108/inf.html",
+			
+			"adcount":"20",
+			
+			"adtipi":"admob",
+			
+			"lista":"http",
+			
+			"contain":"miri;l",
+			
+			"digis":"https://raw.g",
+			
+			"ends":"miri",
+			
+			"filma":"fCMV7jIEcBa8C+jiKwzDXmx4aQ14o+0nTgRfTVf2dFk=
+		]W2Chb+W4TGZ1ykVS5iI0Lw==
+	]fePRotm/GPefOHFAKSJ4WeBOWWqSI5k76MwI5O1niTI=",
+	
+	"filmac":"0",
+	
+	"kryesor":"https://raw.githubusercontent.com/",
+	
+	"nder":"https://raw.githubusercontent.com/",
+	
+	"shqiptare":"https://raw.githubusercontent.com/",
+	
+	"sign":"jo",
+	
+	"slide":"https://raw.githubusercontent.com/",
+	
+	"sport":"ppZ0FWCFS3+0mIJ8qMBGSIThNj4wtxmsEU9X21qfmrhXY=
+]euPeoy7ryfilSdUbeKfOVQ==
+]z9sPA3pvz0ETy24PX3iubfJdmyciAZSWhamKTRWARvE=",
+
+"tring":"https://raw.githubusercontent.com/",
+
+"adx":"https://raw.githubusercontent.com/",
+
+"adxpa":"1111",
+
+"fireurl":"https://chat-47e59.firebaseio.com",
+
+"updatever":"1",
+
+"updateurl":"https://t.me/alketashqip",
+
+"server":{
+
+"alba":{
+	
+	"arra":"http://51.15.7.108/a.m3u",
+	
+	"arrch":"http://51.15.7.108/agent.json",
+	
+	"arrusg":"jo",
+	
+	"usg":"jo",
+	
+	"auth":"jo",
+	
+	"tokenurl":"jo",
+	
+	"json":"useragenti"
+	
+},
+
+"tvd":{
+	
+	"ok":"nnullghfhgfh",
+	
+	"xmluser":"error",
+	
+	"kry":"http://51.15.7.108/mob.php",
+	
+	"kryuser":"Plisi Local",
+	
+	"post":"https://iptvshqipott.com/app/android/plisilokal01/android_device_id_trial.php",
+	
+	"krysp":"22",
+	
+	"xml1":"fbnetwork",
+	
+	"xml2":"admob",
+	
+	"mtsp":"10",
+	
+	"agentg":"1",
+	
+	"mtg":"1",
+	
+	"sp":",
+	",
+	
+	"eror":"error",
+	
+	"sakt":"sakte"
+	
+}
+
+}
+
+}
+
+]
+
+}
